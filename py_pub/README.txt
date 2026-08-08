17a834165806de77e3eaf8f453cfd28a8ee2d110
yajattikam <yajat.a.tikam@gmail.com>
2026-08-08 10:33:42 +0530

    Reorganize repository structure with py_pub and py_sub packages.

---
+++ py_pub/README.txt
@@ -0,0 +1,2 @@
+I have made a Git repository for basic custom made ROS2 nodes. Which can be used as boilerplates. The file py_pub has a basic publisher node, pacakge created with ament_python.
+
---
+++ py_pub/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'py_pub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yajattikam',
+    maintainer_email='yajat.a.tikam@gmail.com',
+    description='Basic publisher node.: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = py_pub.publisher_member_function:main',
+        ],
+    },
+)
